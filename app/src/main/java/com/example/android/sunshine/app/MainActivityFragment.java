6b537810e877d552684a9d2cafd94b50b07c5114
Marko Šojat <markosojat1@gmail.com>
2016-01-07 00:38:16 +0100

    Napravljen adapter za listu.

--- app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
+++ app/src/main/java/com/example/android/sunshine/app/MainActivityFragment.java
@@ -1,10 +1,19 @@
 package com.example.android.sunshine.app;
 
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.List;
+import java.util.ListIterator;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +26,29 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        String[] forecastArray = {
+                "Today - Sunny - 88/64",
+                "Tomorrow - Foggy - 70/40",
+                "Weds - Cloudy - 72/63",
+                "Thurs - Asteroids - 75/65",
+                "Fri - Heavy Rain - 65/56",
+                "Sat - Sunny - 60/51",
+                "Sun - Sunny - 80/68"
+        };
+
+        List<String> weekForecast = new ArrayList<String>(Arrays.asList(forecastArray));
+
+        ArrayAdapter<String> mForecastAdapter = new ArrayAdapter<String>(
+                //The current context
+                getActivity(),
+                //ID of list item layout
+                R.layout.list_item_forecast,
+                //ID of textView to populate
+                R.id.list_item_forecast_textview,
+                //Forecast data
+                weekForecast);
+
         return inflater.inflate(R.layout.fragment_main, container, false);
     }
 }
